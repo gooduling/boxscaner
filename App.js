40b210aa8726c88fadca65151311423c31fa1ad6
VasylenkoStanislav <82873051+VasylenkoStanislav@users.noreply.github.com>
2022-03-18 11:38:21 +0200

    Added RN Camera package

--- App.js
+++ App.js
@@ -6,152 +6,60 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {Node} from 'react';
-import {
-  Platform,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import axios from 'axios';
-
-const baseURL = 'https://jsonplaceholder.typicode.com/posts/1/34';
-
-const MyStatusBar = ({backgroundColor, ...props}) => (
-  <View style={[styles.statusBar, {backgroundColor}]}>
-    <SafeAreaView>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-    </SafeAreaView>
-  </View>
-);
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [post, setPost] = React.useState(null);
-  const test = async () => {
-    try {
-      const response = await axios.get(baseURL);
-      setPost(response.data);
-    } catch (e) {
-      throw e('error==>', e);
-    }
-  };
-
-  // React.useEffect(() => {
-  //   test();
-  // }, []);
-  // console.log('post', post);
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {StyleSheet, Text, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [ocrElements, setOcrElements] = useState([]);
+  const textRecognized = ({textBlocks}) => {
+    const textArray = [];
+    textBlocks.forEach(textBlock => {
+      textBlock.components.forEach(textLine => {
+        textArray.push({
+          bounds: textLine.bounds,
+          text: textLine.value,
+        });
+      });
+    });
+    setOcrElements(textArray);
   };
 
-  // if (Platform.OS === 'ios') {
-  //   StatusBar.setBarStyle('light-content', true); //<<--- add this
-  // }
+  const renderOcrElement = element => {
+    return (
+      <View style={styles.textElement(element)}>
+        <Text>{element.text}</Text>
+      </View>
+    );
+  };
 
   return (
     <View style={{flex: 1}}>
-      <MyStatusBar backgroundColor="#5E8D48" barStyle="light-content" />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <RNCamera
+        style={{flex: 1}}
+        onTextRecognized={textRecognized}
+        captureAudio={false}>
+        {ocrElements?.map(element => renderOcrElement(element))}
+      </RNCamera>
     </View>
   );
 };
 
-const STATUSBAR_HEIGHT = StatusBar.currentHeight;
-const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
-
 const styles = StyleSheet.create({
-  statusBar: {
-    height: STATUSBAR_HEIGHT,
-  },
-  appBar: {
-    backgroundColor: '#79B45D',
-    height: APPBAR_HEIGHT,
-  },
   content: {
     flex: 1,
     backgroundColor: '#33373B',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  textElement: el => ({
+    borderWidth: 1,
+    borderColor: 'blue',
+    position: 'absolute',
+    left: 0,
+    top: el.bounds.origin.y,
+    right: 0,
+    bottom: 50,
+  }),
 });
 
 export default App;
